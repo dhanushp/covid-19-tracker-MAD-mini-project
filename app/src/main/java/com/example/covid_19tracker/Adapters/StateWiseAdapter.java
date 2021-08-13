cdbe0bb88fa0e700de71bc0cd35bdd5ca37aa44f
Sebastian <sebastianthattil2000@gmail.com>
2021-08-13 14:33:18 +0530

    Added Statewise and Districtwise data

--- app/src/main/java/com/example/covid_19tracker/Adapters/StateWiseAdapter.java
+++ app/src/main/java/com/example/covid_19tracker/Adapters/StateWiseAdapter.java
@@ -1,7 +1,22 @@
 package com.example.covid_19tracker.Adapters;
 
+import static com.example.covid_19tracker.Constants.STATE_ACTIVE;
+import static com.example.covid_19tracker.Constants.STATE_CONFIRMED;
+import static com.example.covid_19tracker.Constants.STATE_CONFIRMED_NEW;
+import static com.example.covid_19tracker.Constants.STATE_DEATH;
+import static com.example.covid_19tracker.Constants.STATE_DEATH_NEW;
+import static com.example.covid_19tracker.Constants.STATE_LAST_UPDATE;
+import static com.example.covid_19tracker.Constants.STATE_NAME;
+import static com.example.covid_19tracker.Constants.STATE_RECOVERED;
+import static com.example.covid_19tracker.Constants.STATE_RECOVERED_NEW;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +26,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.covid_19tracker.EachStateDataActivity;
 import com.example.covid_19tracker.Models.StateWiseModel;
 import com.example.covid_19tracker.R;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StateWiseAdapter extends RecyclerView.Adapter<StateWiseAdapter.ViewHolder>
 {
@@ -37,12 +55,44 @@ public class StateWiseAdapter extends RecyclerView.Adapter<StateWiseAdapter.View
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StateWiseAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull StateWiseAdapter.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
         StateWiseModel currentItem = stateWiseModelArrayList.get(position);
         String stateName = currentItem.getState();
         String stateTotal = currentItem.getConfirmed();
         holder.tv_stateTotalCases.setText(NumberFormat.getInstance().format(Integer.parseInt(stateTotal)));
-        holder.tv_stateName.setText(stateName);
+        //holder.tv_stateName.setText(stateName);
+        if(searchText.length()>0){
+            //color your text here
+            sb = new SpannableStringBuilder(stateName);
+            Pattern word = Pattern.compile(searchText.toLowerCase());
+            Matcher match = word.matcher(stateName.toLowerCase());
+            while(match.find()){
+                ForegroundColorSpan fcs = new ForegroundColorSpan(Color.rgb(52, 195, 235)); //specify color here
+                sb.setSpan(fcs, match.start(), match.end(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+            }
+            holder.tv_stateName.setText(sb);
+
+        }else{
+            holder.tv_stateName.setText(stateName);
+        }
+        holder.lin_state_wise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StateWiseModel clickedItem = stateWiseModelArrayList.get(position);
+                Intent perStateIntent = new Intent(mContext, EachStateDataActivity.class);
+                perStateIntent.putExtra(STATE_NAME, clickedItem.getState());
+                perStateIntent.putExtra(STATE_CONFIRMED, clickedItem.getConfirmed());
+                perStateIntent.putExtra(STATE_CONFIRMED_NEW, clickedItem.getConfirmed_new());
+                perStateIntent.putExtra(STATE_ACTIVE, clickedItem.getActive());
+                perStateIntent.putExtra(STATE_DEATH, clickedItem.getDeath());
+                perStateIntent.putExtra(STATE_DEATH_NEW, clickedItem.getDeath_new());
+                perStateIntent.putExtra(STATE_RECOVERED, clickedItem.getRecovered());
+                perStateIntent.putExtra(STATE_RECOVERED_NEW, clickedItem.getRecovered_new());
+                perStateIntent.putExtra(STATE_LAST_UPDATE, clickedItem.getLastupdate());
+
+                mContext.startActivity(perStateIntent);
+            }
+        });
 
     }
 
